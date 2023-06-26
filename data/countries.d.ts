59e54754e34c09e376b793e79e080144d10b2b4d
Michael Labriola <labriola@acm.org>
2023-06-26 18:09:19 -0500

    built

---
+++ data/countries.d.ts
@@ -0,0 +1,3 @@
+import type { Countries } from "../.internal/Data.js";
+declare const countries: Countries;
+export default countries;
---
+++ lang/HE.d.ts
@@ -0,0 +1,3 @@
+import type { Lang } from "../.internal/Lang.js";
+declare const lang: Lang;
+export default lang;
